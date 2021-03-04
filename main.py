32f0ee6a5362067a090c25237b4c41978f172d01
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-03-04 10:45:08 +0800

    Removed Stream and made it into a Cog

--- main.py
+++ main.py
@@ -309,45 +309,6 @@ async def queue(ctx, url: str):
 
     print("Song added to queue\n")
 
-
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'quiet': True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}   
-
-ffmpeg_options = {
-'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ydl_opts)
-class stream(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-    @commands.command()
-    async def stream(self, ctx, *, url):
-        """Streams from a url (same as yt, but doesn't predownload)"""
-        player = await stream.from_url(url, loop=client.loop, stream=True)
-        ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-        await ctx.send('Now playing: {}'.format(player.title))
-
 @client.command()
 async def load(ctx, extension): 
     client.load_extension(f'cogs.{extension}')
